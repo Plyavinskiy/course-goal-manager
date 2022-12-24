0f1434f7979ebad4461b277353bdf42905bb3cf1
Maksim Pliavinski <pliavinski.maksim@gmail.com>
2022-12-24 10:39:46 +0200

    feat: implement displaying a list of course goals

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { useState } from "react";
+
+import CourseGoalList from "./components/CourseGoals/CourseGoalList/CourseGoalList";
 import "./App.css";
 
 const App = () => {
-  return <div>Getting Started</div>;
+  const [courseGoals, setCourseGoals] = useState([
+    { text: "Do all exercises!", id: "g1" },
+    { text: "Finish the course!", id: "g2" },
+  ]);
+
+  return (
+    <div>
+      <section id="goals">
+        <CourseGoalList items={courseGoals} />
+      </section>
+    </div>
+  );
 };
 
 export default App;
