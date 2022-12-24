8397ea5193598a5da4ea2419b7741fe3c9c37f6b
Maksim Pliavinski <pliavinski.maksim@gmail.com>
2022-12-24 10:53:37 +0200

    feat: notify the user that there are no course goals

--- src/App.js
+++ src/App.js
@@ -9,11 +9,17 @@ const App = () => {
     { text: "Finish the course!", id: "g2" },
   ]);
 
+  let content = (
+    <p style={{ textAlign: "center" }}>No goals found. Maybe add one?</p>
+  );
+
+  if (courseGoals.length > 0) {
+    content = <CourseGoalList items={courseGoals} />;
+  }
+
   return (
     <div>
-      <section id="goals">
-        <CourseGoalList items={courseGoals} />
-      </section>
+      <section id="goals">{content}</section>
     </div>
   );
 };
